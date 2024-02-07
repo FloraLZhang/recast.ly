3be7ba35c52b4b2b826008056288f5bb35808de5
Flora Zhang <Azurstyle@outlook.com>
2024-02-06 23:17:14 -0500

    create Youtube API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -4,8 +4,21 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
 
-var searchYouTube = (query, callback) => {
+var searchYouTube = (query, callback, errorCB = null) => {
   // TODO
+  const url ='https://app-hrsei-api.herokuapp.com/api/recastly/videos';
+  $.ajax({
+    url: url,
+    type: 'GET',
+    data: {
+      q: query,
+      youtube_api_key:YOUTUBE_API_KEY,
+    },
+    success: (data) => {
+      callback(data);
+    },
+    error: errorCB  || function(response) { console.log('Youtube Search Error:', response)}
+  });
 };
 
 export default searchYouTube;
