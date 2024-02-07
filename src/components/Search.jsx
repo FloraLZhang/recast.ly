46c14fe4fa58d2cd88e36a89fb5f8f02d3009334
Flora Zhang <Azurstyle@outlook.com>
2024-02-06 23:14:13 -0500

    Implement live search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,23 @@
-var Search = () => (
+const { useState } = React;
+var Search = (props) => {
+  const [input, setInput] = useState("");
+  const handleInputChange =(event) => {
+    props.handleSearch(event.target.value);
+    setInput(event.target.value);
+  }
+
+  return (
   <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
+    <input className="form-control"
+    type="text"
+    value ={input}
+    onChange={handleInputChange} />
     <button className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
     </button>
-  </div> 
-);
-
+  </div>
+  );
+};
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default Search;
