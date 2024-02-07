226342ee09fc3fa8497e2f90599d46a7b438fbc1
Flora Zhang <Azurstyle@outlook.com>
2024-02-06 23:13:50 -0500

    Render App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,54 @@
-import exampleVideoData from "../data/exampleVideoData.js";
-import VideoList from "./VideoList.js";
+import Search from "./Search.js";
+import searchYouTube from "../lib/searchYouTube.js";
 import VideoPlayer from "./VideoPlayer.js";
+import VideoList from "./VideoList.js";
 
-var App = (props) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData} />
+const { useState, useEffect } = React;
+
+var App = (props) => {
+  const [currentVideo, setCurrentVideo] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const getSearchVideos = (input) => {
+    searchYouTube(input, (videos) => {
+      setCurrentVideo(videos[0]);
+      setVideos(videos);
+    });
+  };
+
+  const handleVideoClick = (video) => {
+    setCurrentVideo(video)
+  };
+
+  useEffect(() => {
+    getSearchVideos("");
+  }, []);
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <h5><em>Search</em>
+            <Search handleSearch={getSearchVideos} /></h5>
+          </div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <h5><em>VideoPlayer</em>
+          <VideoPlayer video={currentVideo} /></h5>
+        </div>
+        <div className="col-md-5">
+        <h5><em>VideoList</em>
+          <VideoList
+            videos={videos} onVideoClick={handleVideoClick}
+          /></h5>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
